8e16c2fc83a49f8a1533468a153956dd32408bbf
ztmxz <T.Erehovec@belaz.minsk.by>
2025-11-06 12:27:16 +0300

    Add hooks

--- start.cjs
+++ start.cjs
@@ -5,6 +5,20 @@ const serverOptions = {
 
 const app = fastify(serverOptions);
 
+app.addHook("onRoute", function inspector(routeOptions) {
+  console.log(routeOptions);
+});
+app.addHook("onRegister", function inspector(plugin, pluginOptions) {
+  console.log("Chapter 2, Plugin System and Boot Process");
+});
+app.addHook("onReady", async function preLoading() {
+  console.log("onReady");
+});
+app.addHook("onClose", function manageClose(done) {
+  console.log("onClose");
+  done();
+});
+
 app
   .listen({
     port: 8000,
